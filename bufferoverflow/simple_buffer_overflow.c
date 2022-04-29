a776225d5969269b2a1929988c74159997326036
fanbyprinciple <fanbyprinciple@gmail.com>
2022-04-29 17:19:16 +0530

    added more stuff

--- bufferoverflow/simple_buffer_overflow.c
+++ bufferoverflow/simple_buffer_overflow.c
@@ -1,16 +1,28 @@
+/*
+gcc -mpreferred-stack-boundary=3 -ggdb -fno-stack-protector auth_overflow.c -o auth_overflow
+
+installing gef - bash -c "$(wget https://gef.blah.cat/sh -O -)"
+
+*/
+
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 
 int check_authentication(char *password){
-    int auth_falg = 0;
-    char passwordbuffer[16];
+    int auth_flag = 0;
+    char password_buffer[16];
 
     strcpy(password_buffer, password);
 
-    if (strcmp(password_buffer, "ashwin is nice")==0){
+    if (strcmp(password_buffer, "ashwin")==0){
         auth_flag = 1;
-    }
+    } 
+
+    //printf("%d, %d", auth_flag, strcmp(password_buffer, "ashwin"));
+    
+    return auth_flag;
 }
 
 int main(int argc, char *argv[]) {
@@ -21,11 +33,11 @@ int main(int argc, char *argv[]) {
 
     if(check_authentication(argv[1])){
         printf("\n-=-=-=-=-=-=-=-=-=-=-=-\n");
-        printf("    Access Denied   ");
+        printf("    Access Granted   ");
         printf("\n-=-=-=-=-=-=-=-=-=-=-=-\n");
 
     } else {
-        printf("\n-=-=-= Access Denied. =-=-=-\n")
+        printf("\n-=-=-= Access Denied. =-=-=-\n");
     }
 
 }
