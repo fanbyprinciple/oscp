3f9543a9d7669d9806e59b69c31367c4fcc4ff8e
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2023-05-12 15:46:32 +0530

    for tweeting directly

--- tweeting_to_twitter.py
+++ tweeting_to_twitter.py
@@ -1,7 +1,7 @@
-consumer_key        = ''
-consumer_secret     = ''
-access_token        = ''
-access_token_secret = ''
+consumer_key        = 'first from the secret.txt'
+consumer_secret     = 'second from the secret.txt'
+access_token        = 'third from the secret.txt'
+access_token_secret = 'fourth from the secret.txt'
 
 
 
